f895485b910dfbe22047cf050aa6b9927b237315
Marcus Valtonen Örnhag <marcusvaltonen@gmail.com>
2022-04-05 22:26:20 +0200

    wip: card indicator

--- pyproximal/proximal/QuadraticEnvelope.py
+++ pyproximal/proximal/QuadraticEnvelope.py
@@ -86,3 +86,82 @@ class QuadraticEnvelopeCard(ProxOperator):
         else:
             r[idx] = np.maximum(0, (r[idx] - tau * np.sqrt(2 * self.mu)) / (1 - tau))
         return r * np.sign(x)
+
+
+class QuadraticEnvelopeCardIndicator(ProxOperator):
+    r"""Quadratic envelope of the indicator function of :math:`\ell_0`-penalty.
+
+    The :math:`\ell_0`-penalty is also known as the *cardinality function*, and the
+    quadratic envelope :math:`\mathcal{Q}(\mu\|\cdot\|_0)` of it is defined as
+
+    .. math::
+
+        \mathcal{Q}(\mu\|\cdot\|_0)(x) = \sum_i \left(\mu - \frac{1}{2}\max(0, \sqrt{2\mu} - |x_i|)^2\right)
+
+    where :math:`\mu \geq 0`.
+
+    Parameters
+    ----------
+    r0 : :obj:`int`
+        Threshold parameter.
+
+    Notes
+    -----
+    The terminology *quadratic envelope* was coined in [1]_, however, the rationale has
+    been used earlier, e.g. in [2]_. In a general setting, the quadratic envelope
+    :math:`\mathcal{Q}(f)(x)` is defined such that
+
+    .. math::
+
+        \left(f(x) + \frac{1}{2}\|x-y\|_2^2\right)^{**} = \mathcal{Q}(f)(x) + \frac{1}{2}\|x-y\|_2^2
+
+    where :math:`g^{**}` denotes the bi-conjugate of :math:`g`, which is the l.s.c.
+    convex envelope of :math:`g`.
+
+    There is no closed-form expression for :math:`\mathcal{Q}(f)(x)` given an arbitrary
+    function :math:`f`. However, for certain special cases, such as in the case of the
+    cardinality function, such expressions do exist.
+
+    The proximal operator is given by
+
+    .. math::
+
+        \prox_{\tau\mathcal{Q}(\mu\|\cdot\|_0)}(x) =
+        \begin{cases}
+        x_i, & |x_i| \geq \sqrt{2 \mu} \\
+        \frac{x_i-\tau\sqrt{2\mu}\sgn(x_i)}{1-\tau}, & \tau\sqrt{2\mu} < |x_i| < \sqrt{2 \mu} \\
+        0, & |x_i| \leq \tau\sqrt{2 \mu}
+        \end{cases}
+
+    By
+
+    .. [1] Carlsson, M. "On Convex Envelopes and Regularization of Non-convex
+        Functionals Without Moving Global Minima", In Journal of Optimization Theory
+        and Applications, 183:66–84, 2019.
+    .. [2] Larsson, V. and Olsson, C. "Convex Low Rank Approximation", In International
+        Journal of Computer Vision (IJCV), 120:194–214, 2016.
+
+    """
+
+    def __init__(self, r0):
+        super().__init__(None, False)
+        self.r0 = r0
+
+    def __call__(self, x):
+        return np.sum(self.elementwise(x))
+
+    def elementwise(self, x):
+        #TODO: Implement this
+        return self.mu - 0.5 * np.maximum(0, np.sqrt(2 * self.mu) - np.abs(x)) ** 2
+
+    @_check_tau
+    def prox(self, x, tau):
+        #TODO: Implement this
+
+        r = np.abs(x)
+        idx = r < np.sqrt(2 * self.mu)
+        if tau >= 1:
+            r[idx] = 0
+        else:
+            r[idx] = np.maximum(0, (r[idx] - tau * np.sqrt(2 * self.mu)) / (1 - tau))
+        return r * np.sign(x)
