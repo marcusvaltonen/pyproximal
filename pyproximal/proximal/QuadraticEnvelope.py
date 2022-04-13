04f25eb8310703d695761a49b914ccb84c706ed0
Marcus Valtonen Örnhag <marcusvaltonen@gmail.com>
2022-04-13 10:03:25 +0200

    wip

--- pyproximal/proximal/QuadraticEnvelope.py
+++ pyproximal/proximal/QuadraticEnvelope.py
@@ -92,13 +92,25 @@ class QuadraticEnvelopeCardIndicator(ProxOperator):
     r"""Quadratic envelope of the indicator function of :math:`\ell_0`-penalty.
 
     The :math:`\ell_0`-penalty is also known as the *cardinality function*, and the
-    quadratic envelope :math:`\mathcal{Q}(\mu\|\cdot\|_0)` of it is defined as
+    indicator function is
 
     .. math::
 
-        \mathcal{Q}(\mu\|\cdot\|_0)(x) = \sum_i \left(\mu - \frac{1}{2}\max(0, \sqrt{2\mu} - |x_i|)^2\right)
+        \mathcal{I}_{r_0}(\mathbf{x}) = \mathcal{I}_{\|\cdot\|_0\leq r_0}(\mathbf{x}) =
+        \begin{cases}
+        0, & \mathbf{x}\leq r_0 \\
+        \infty, & \text{otherwise}
+        \end{cases}
 
-    where :math:`\mu \geq 0`.
+    The quadratic envelope :math:`\mathcal{Q}(\mathcal{I}_{r_0})` can be written as
+
+    .. math::
+
+        \mathcal{Q}(\mathcal{I}_{r_0})(x) =
+        \frac{1}{k^*}\left(\sum_{i>r_0-k^*}|x_i|\right)^2 - \left(\sum_{i>r_0-k^*}|x_i|^2
+
+    where :math:`r_0 \geq 0`, and :math:`k^* \leq r_0`, see [3]_ for details. There are
+    other, equivalent ways, of writing this penalty, see e.g. [1]_ and [2]_.
 
     Parameters
     ----------
@@ -120,7 +132,7 @@ class QuadraticEnvelopeCardIndicator(ProxOperator):
 
     There is no closed-form expression for :math:`\mathcal{Q}(f)(x)` given an arbitrary
     function :math:`f`. However, for certain special cases, such as in the case of the
-    cardinality function, such expressions do exist.
+    indicator function of the cardinality function, such expressions do exist.
 
     The proximal operator is given by
 
@@ -133,13 +145,15 @@ class QuadraticEnvelopeCardIndicator(ProxOperator):
         0, & |x_i| \leq \tau\sqrt{2 \mu}
         \end{cases}
 
-    By
+    Note that this is a non-separable penalty.
 
     .. [1] Carlsson, M. "On Convex Envelopes and Regularization of Non-convex
         Functionals Without Moving Global Minima", In Journal of Optimization Theory
         and Applications, 183:66–84, 2019.
     .. [2] Larsson, V. and Olsson, C. "Convex Low Rank Approximation", In International
         Journal of Computer Vision (IJCV), 120:194–214, 2016.
+    .. [3] Andersson et al. "Convex envelopes for fixed rank approximation", In
+        Optimization Letters, 11:1783–1795, 2017.
 
     """
 
@@ -148,11 +162,39 @@ class QuadraticEnvelopeCardIndicator(ProxOperator):
         self.r0 = r0
 
     def __call__(self, x):
-        return np.sum(self.elementwise(x))
+        #TODO: Implement this (non-separable), so elementwise not possible
+        #TODO: write a hankel recovery tutorial.
+        """
+        function[out, putative, ell] = Rr0_mod(X, r0)
 
-    def elementwise(self, x):
-        #TODO: Implement this
-        return self.mu - 0.5 * np.maximum(0, np.sqrt(2 * self.mu) - np.abs(x)) ** 2
+        if size(X, 2) > 1
+            sing = svd(X, 'econ');
+        else
+            sing = X;
+        end
+
+        sums = cumsum(X, 'reverse');
+        sings_ext = [inf;
+        sing;
+        0];
+
+        ell = 0;
+        for j = 1:r0
+        putative = 1 / (r0 - j + 1) * sums(j);
+        if sings_ext(j) >= putative & & putative >= sings_ext(j + 1)
+            ell = j - 1;
+            break;
+        end
+
+    end
+
+    % n = length(sing);
+    % out = (n - r0) * putative ^ 2 - sum((putative - sing(ell + 1:end)). ^ 2);
+    % out = 2 * putative * sum(sing(ell + 1:end)) - sum(sing(ell + 1: end).^ 2)-(r0 - ell) * putative ^ 2
+    out = 1 / (r0 - ell) * sum(sing(ell + 1:end)) ^ 2 - sum(sing(ell + 1: end).^ 2);
+
+"""
+    pass
 
     @_check_tau
     def prox(self, x, tau):
